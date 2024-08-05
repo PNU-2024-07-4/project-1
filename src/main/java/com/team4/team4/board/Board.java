ba33e7535c16807b3a1c4ab1ea22efb9bdd45d73
Ui-Cheol Shin <suc2150@gmail.com>
2024-08-05 14:49:51 +0900

    #23 entitiy modeling (#26)
    * entity modeling
    
    * content length change
    
    * ãmatch variable name

---
+++ src/main/java/com/team4/team4/board/Board.java
@@ -0,0 +1,50 @@
+package com.team4.team4.board;
+
+import com.team4.team4.comment.Comment;
+import com.team4.team4.user.SiteUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Board {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "userId", nullable = false)
+  private SiteUser user;
+
+  @Column(nullable = false, length = 50)
+  private String subject;
+
+  @Column(nullable = false, length = 255)
+  private String content;
+
+  @Column(nullable = false)
+  private LocalDateTime startDay;
+
+  @Column(nullable = false)
+  private LocalDateTime endDay;
+
+  @Column(nullable = false, length = 50)
+  private String region;
+
+  @Column(nullable = false)
+  private int recruitNumber;
+
+  private int currentNumber;
+
+  @Column(length = 50)
+  private String recommendedTo;
+
+  @OneToMany(mappedBy = "board")
+  private List<Comment> comments;
+
+}
---
+++ src/main/java/com/team4/team4/comment/Comment.java
@@ -0,0 +1,29 @@
+package com.team4.team4.comment;
+
+import com.team4.team4.board.Board;
+import com.team4.team4.user.SiteUser;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "userId", nullable = false)
+  private SiteUser user;
+
+  @ManyToOne
+  @JoinColumn(name = "boardId", nullable = false)
+  private Board board;
+
+  @Column(nullable = false)
+  private String content;
+
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
+
+}
