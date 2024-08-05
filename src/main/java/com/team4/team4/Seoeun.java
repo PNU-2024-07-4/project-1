742e06310402f315d8c67b79dd381791553b04c5
jinwon1234 <ljw0626@naver.com>
2024-08-05 13:22:11 +0900

    work

--- src/main/java/com/team4/team4/Seoeun.java
+++
@@ -1,7 +0,0 @@
-package com.team4.team4;
-
-public class Seoeun {
-    public static void main(String[] args) {
-        System.out.println("hello seoeun");
-    }
-}
--- src/main/java/com/team4/team4/Uicheol.java
+++
@@ -1,7 +0,0 @@
-package com.team4.team4;
-
-public class Uicheol {
-  public static void main(String[] args) {
-    System.out.println("uicheol");
-  }
-}
