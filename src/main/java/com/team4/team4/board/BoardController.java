f16caaf066254a2b09e61e42f8d210ac390706b4
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2024-08-06 16:03:44 +0900

    board 생성 html (#37)
    * board 생성 html
    
    * controller 포함

--- src/main/java/com/team4/team4/board/BoardController.java
+++ src/main/java/com/team4/team4/board/BoardController.java
@@ -1,5 +1,20 @@
 package com.team4.team4.board;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
 public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/create")
+    public String createBoard() {
+        return "board_create_form";
+    }
+
 }
---
+++ src/main/java/com/team4/team4/board/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.team4.team4.board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository <Board, Long> {
+
+}
