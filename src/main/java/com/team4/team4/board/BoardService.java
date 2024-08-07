da18835a4b95e8fcc2f146942bc20de6d65b449f
jinwon1234 <ljw0626@naver.com>
2024-08-07 09:50:40 +0900

    work

--- src/main/java/com/team4/team4/board/BoardService.java
+++ src/main/java/com/team4/team4/board/BoardService.java
@@ -1,13 +1,23 @@
 package com.team4.team4.board;
 
+import com.team4.team4.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class BoardService {
 
     private final BoardRepository boardRepository;
-
+    public Board getBoard(Long id) {
+        Optional<Board> board = this.boardRepository.findById(id);
+        if(board.isPresent()) {
+            return board.get();
+        }else {
+            throw new DataNotFoundException("Board not found");
+        }
+    }
 
 }
