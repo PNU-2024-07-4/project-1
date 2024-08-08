b88a6247de47c398f4b56b9830e2a2b706717128
jhs512 <jangka512@gmail.com>
2024-08-08 16:18:24 +0900

    work (#69)

--- src/main/java/com/team4/team4/MainController.java
+++ src/main/java/com/team4/team4/MainController.java
@@ -1,13 +1,22 @@
 package com.team4.team4;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    @Value("${custom.site.secretName}")
+    private String siteSecretName;
 
+    @GetMapping("/secret")
+    @ResponseBody
+    public String showSecret(Model model) {
+        return "The secret name is: " + siteSecretName;
+    }
 
     @GetMapping("/")
     public String mainBoard(Model model) {
